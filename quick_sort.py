df556387923ab046d35871ddb85c8016672c952d
Dae-Ki Kang <dkkang@gmail.com>
2023-11-18 15:37:36 +0900

    modified:   quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,8 +1,20 @@
 from typing import List
 def partition(A:List[int], lo:int, hi:int)->List[int]:
-    i=hi-1
-    
-    return i
+    i=lo
+    j=hi-1
+    pivot=A[hi]
+    while i<j:
+        while A[i]<pivot and i<j: 
+            i+=1
+        while A[j]>=pivot and i<j: 
+            j-=1
+        if i<j:
+            A[i],A[j]=A[j],A[i]
+    if A[j]>pivot:
+        A[j],A[hi]=A[hi],A[j]
+        return j
+    else:
+        return j+1
 
 def quick_sort(A:List[int], lo:int, hi:int)->List[int]:
     if lo<hi:
@@ -11,8 +23,9 @@ def quick_sort(A:List[int], lo:int, hi:int)->List[int]:
         quick_sort(A,pivot+1,hi)
     return A
 
+n=1000
+b=1000
 import random
-A = [random.randint(1,100) for i in range(100)]
+A = [random.randint(1,b) for i in range(n)]
 print(A)
-print(quick_sort(A,1,100))
-print(A)
+print(quick_sort(A,0,n-1))
